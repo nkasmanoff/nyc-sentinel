f17c7ec9bc8e412b6541c15635283f58d2ffb233
Noah Kasmanoff <nkasmanoff@gmail.com>
2022-09-26 20:25:06 -0400

    add: script to fix when .git gets corrupted

---
+++ reset_corrupt_git.sh
@@ -0,0 +1,8 @@
+rm -fr .git
+git init
+git remote add origin https://github.com/nkasmanoff/nyc-sentinel
+git fetch
+git reset --mixed origin/main
+git checkout -b main
+git branch -D master
+git branch --set-upstream-to=origin/main main
